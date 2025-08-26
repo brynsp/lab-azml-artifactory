07f475b2b0a8d6989f9c49a66f2219043ea170ee
Bryn Spears <bryn.spears@hotmail.com>
2025-08-26 13:37:15 -0400

    moving rg declaration into main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,12 @@
+module "rg" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = ">= 0.1.0"
+
+  name             = var.rg_name
+  location         = var.location
+  enable_telemetry = false
+}
+
 module "kv" {
   source  = "Azure/avm-res-keyvault-vault/azurerm"
   version = ">= 0.1.0"
